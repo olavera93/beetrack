d117dbab878c92f58f084f65afa18d4e98e3b758
Carlos Arevalo <olavera93@gmail.com>
2025-06-06 19:43:52 -0500

    Base servidor

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def upload_file():
         return send_file(output_file, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
